723ec4ff5ff5ffdd3716b5fd69b15b259ee4f7fd
AngelRRand <horacio27rodriguez_cba@hotmail.com>
2023-08-01 09:45:31 -0300

    a

---
+++ Clase 1/index.py
@@ -0,0 +1,29 @@
+""" Ejercicio 1 """
+print('Hola mundo')
+print('Gente hermosa\nGente bella')
+print('\t Ñam ñam') 
+
+""" Ejercicio 2 """
+
+print('A\tB\tC\nD\tE\tF\nG\tH\tI')
+
+""" Ejercicio 3 """
+
+print('Barra Normal: /\nBarra Invewrtida: \\')
+
+""" Ejercicio 4 """
+
+""" 
+print(input('Dime tu nombre:'))
+print(input('Dime tu apellido:'))
+ """
+
+
+""" 
+print(input('¿Que estas estudiando?'))
+print(input('¿En que pais vives?'))
+print(input('Dime tu nombre') + ' ' + input('Dime tu apellido'))
+
+ """
+
+print('El nombre de tu cerveza es:\n"' + input('Dime cual es tu color favorito ') + ' ' + input('Dime de que pais eres ') + '"')
