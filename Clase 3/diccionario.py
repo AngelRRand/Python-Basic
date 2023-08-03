eb1faac6ca97ec95cf131240e0a1274847269205
AngelRRand <horacio27rodriguez_cba@hotmail.com>
2023-08-03 13:57:56 -0300

    a

--- Clase 3/diccionario.py
+++ Clase 3/diccionario.py
@@ -1,5 +1,22 @@
-
 dic = {"c1": ["a", "b", "c"], "c2": ["d", "e", "f"]}
 
 print(dic["c2"][1].upper())
 
+mi_dic = {
+    "nombre": "Karen",
+    "apellido": "Jurgens",
+    "edad": 35,
+    "ocupacion": "Periodista",
+}
+
+
+mi_dict = {
+    "valores_1": {"v1": 3, "v2": 6},
+    "puntos": {"points1": 9, "points2": [10, 300, 15]},
+}
+
+print(mi_dict["puntos"]["points2"][1])
+
+
+mi_dic["pais"] = "Colombia"
+print(mi_dic)
---
+++ Clase 3/tubles.py
@@ -0,0 +1,19 @@
+a = 'hola'
+print(a[0])
+
+
+mi_tupla = (1, 2, 3, 2, 3, 1, 3, 2, 3, 3, 3, 1, 3, 2, 2, 1, 3, 2)
+
+print(mi_tupla.count(2))
+
+mi_tupla = (1, 2, 3, 2, 3, 1, 3, 2)
+
+mi_lista = list(mi_tupla)
+
+mi_tupla = (1, 2, 3, 4)
+
+a = mi_tupla[0]
+b = mi_tupla[1]
+c = mi_tupla[2]
+d = mi_tupla[3]
+
