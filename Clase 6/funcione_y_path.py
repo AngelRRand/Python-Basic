02980a0c691624ce64dddae1cf725534ad52b94c
AngelRRand <horacio27rodriguez_cba@hotmail.com>
2023-08-06 13:06:57 -0300

    aaasda

---
+++ Clase 6/funcione_y_path.py
@@ -0,0 +1,47 @@
+from pathlib import Path
+from os import system
+
+system("cls")
+
+""" Ejercicio 1 """
+print("\n")
+
+ruta_base = "C:/Users/Horac/OneDrive/Escritorio/Orasio/Aprendiendo/11 Aprendiendo Python/Python Basic/Clase 6/texto/funciones"
+
+
+def abrir_leer(base):
+
+    ruta = Path(base, "open.txt")
+
+    return ruta.read_text() 
+
+
+print(abrir_leer(ruta_base))
+
+
+""" Ejercicio 2 """
+print("\n")
+
+def sobrescribir(base, archivo):
+    directorio = Path(base, archivo)
+    ruta = open(directorio, "a")
+
+    ruta.write("contenido eliminado")
+
+    ruta.close()
+
+    texto = directorio.read_text() 
+    return texto 
+
+print(sobrescribir(ruta_base, "open.txt"))
+
+
+
+""" Ejercicio 3 """
+print("\n")
+
+
+
+
+
+
