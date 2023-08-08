f72a187b9c0c7bb61e8eb37dcbaa9259550be44d
AngelRRand <horacio27rodriguez_cba@hotmail.com>
2023-08-08 14:06:57 -0300

    proyecto

--- Clase 7/Teoria/metodos_especiales.py
+++ Clase 7/Teoria/metodos_especiales.py
@@ -1,2 +1,29 @@
-mi_lista = list(range(0, 10))
-print(mi_lista)
+""" METODOS ESPECIALES """
+
+class CD:
+    def __init__(self, autor, titulo, canciones):
+        self.autor = autor
+        self.titulo = titulo
+        self.canciones = canciones
+
+    # Modifica la forma en la que se va a ver el objeto.
+    # Usar return
+    def __str__(self):
+        return f"{self.titulo} su autor es {self.autor}"
+    
+    # Devuelve la cantidad del valor que coloquemos
+    def __len__(self):
+        return self.canciones
+    
+    # Modifica la forma en la que se elimina el objeto.
+    def __del__(self):
+        print("Se ha eliminado el cd")
+
+
+
+mi_cd = CD('pink', 'the wall', 24)
+
+print(mi_cd)
+print(len(mi_cd))
+
+del mi_cd
--- Clase 7/metodos_especiales.py
+++ Clase 7/metodos_especiales.py
@@ -1,10 +1,31 @@
 """ Ejercicio 1 """
 print("\n")
 
+class Libro():
+    def __init__(self, titulo, autor, cantidad_paginas):
+        self.titulo = titulo
+        self.autor = autor
+        self.cantidad_paginas = cantidad_paginas
+        
+    def __str__(self):
+        return f"{self.titulo}, de {self.autor}"
+    
+    def __len__(self):
+        return self.cantidad_paginas
+    
+    def __del__(self):
+        print("Libro eliminado")
+
+mi_libro = Libro("Vida de mierda", "Orasio", 1)
+
+print(mi_libro)
 
 """ Ejercicio 2 """
 print("\n")
 
+print(len(mi_libro))
 
 """ Ejercicio 3 """
 print("\n")
+
+del mi_libro
