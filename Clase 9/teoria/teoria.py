d185fb797046c49c9b9c79d448a4c2a021c326f9
AngelRRand <horacio27rodriguez_cba@hotmail.com>
2023-08-21 19:15:49 -0300

    antes de que borre todo

--- Clase 9/teoria/teoria.py
+++ Clase 9/teoria/teoria.py
@@ -1,9 +1,9 @@
 # Saber en que directorio estamos parados 
 import os
 import shutil
-print(os.getcwd(), "holis")
+import send2trash
 
-
+ruta = os.getcwd()
 
 archivo = open("miTexto.txt", "w")
 archivo.write("Texto de prueba")
@@ -11,4 +11,20 @@ archivo.close()
 
 
 
-shutil.move("miTexto.txt", f"{os.getcwd()}/texto") 
+shutil.move("miTexto.txt", f"{ruta}/texto") 
+
+# Mueve el archivo a la papelera
+send2trash.send2trash(f"{ruta}\\texto\\miTexto.txt")
+
+
+for carpeta, subcarpeta, archivo in os.walk(f"{ruta}\\carpeta_padre"):
+    print(f"En la carpeta: {carpeta}")
+    print(f"La subcarpeta es: ")
+    for sub in subcarpeta:
+        print(f"\t{sub}")
+        print(f"Los archivos son: ")
+    for a in archivo:
+        print(f"\t{a}")
+    print("\n")
+
+
