15e47a9c48a093f6c3639c2ddfb93e0f5f7c8bde
AngelRRand <horacio27rodriguez_cba@hotmail.com>
2023-09-05 16:00:31 -0300

    helena

--- Clase 13/index.py
+++ Clase 13/index.py
@@ -9,6 +9,8 @@ import wikipedia
 
 
 
+id1 = "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_EN-US_ZIRA_11.0"
+id2 = "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_ES-ES_HELETokens\TTS_MS_ES-ES_HELENA_11.0"
 
 
 
@@ -22,7 +24,7 @@ def transformar_audio():
     # Configurar el microfono
     with sr.Microphone() as origen:
         # tiempo de espera
-        r.pause = 0.8
+        r.pause = 0.5
 
         # Informar que comenzo la grabacion
         print("Habla")
@@ -66,11 +68,112 @@ def transformar_audio():
 def hablar(mensaje):
 
     engine = pyttsx3.init()
+    engine.setProperty("voice", id2)
 
     engine.say(mensaje)
     engine.runAndWait()
 
 
-hablar("I am Horacio")
+def pedir_dia():
+
+    dia = datetime.date.today()
+
+    dia_semana = dia.weekday()
+
+    calendario = ["lunes", "martes", "miercoles", "jueves", "viernes", "sabado", "domingo"]
+    hablar(f"Hola Horacio. ¿Como estas?, hoy es {calendario[dia_semana]}")
+
+def pedir_Hora():
+    hora = datetime.datetime.now()
+
+    oracion = f"En este momento son las {hora.hour} horas con {hora.minute} minutos"
+
+    hablar(oracion)
+
+def saludo_inicial():
+    hora = datetime.datetime.now()
+    if hora.hour < 6 or hora.hour > 20:
+        momento = "Buenas noches"
+    elif 6 <= hora.hour < 23:
+        momento = "Buen dia"
+    else:
+        momento = "Buenas tardes"
+    hablar(f"{momento} soy Helena, tu asistente personal. Por favor, dime en que te puedo ayudar")
+
+
+def pedir_cosas():
+
+    saludo_inicial()
+
+    comenzar = True
+
+    while comenzar:
+        pedido = transformar_audio().lower()
+
+        if 'abrir youtube' in pedido:
+            hablar("Con gusto, estoy abriendo youtube")
+            webbrowser.open("https://www.youtube.com/")
+            continue
+        elif "abrir navegador" in pedido:
+            hablar("Estoy en eso")
+            webbrowser.open("https://www.google.com/?hl=es")
+            continue
+        elif "qué día es hoy" in pedido:
+            pedir_dia()
+            continue
+        elif "qué hora es" in pedido:
+            pedir_Hora()
+            continue
+        elif "busca en wikipedia" in pedido:
+            hablar("Buscando en wikipedia...")
+            pedido = pedido.replace("busca en wikipedia", "")
+
+            wikipedia.set_lang("es")
+            resultado = wikipedia.summary(pedido, sentences=1)
+
+            hablar("Wikipedia dice lo siguiente: ")
+            hablar(resultado)
+            continue
+        elif "busca en internet" in pedido:
+            hablar("Ya mismo estoy en eso")
+            pedido = pedido.replace("busca en internet", "")
+
+            pywhatkit.search(pedido)
+            hablar("Esto es lo que he encontrado")
+            continue
+        elif "reproducir" in pedido:
+            hablar("Reproduciendo...")
+            pywhatkit.playonyt(pedido)
+            continue
+        elif "broma" in pedido or "chiste" in pedido:
+            hablar(pyjokes.get_joke('es'))
+            continue
+        elif "precio de las acciones" in pedido:
+            accion = pedido.split("de")[-1].strip()
+            cartera =  {
+                "apple":"APPL",
+                "amazon": "AMZN",
+                "google": "GOOGL"
+            }
+            try:
+                accion_buscada = cartera[accion]
+                accion_buscada = yf.Ticker(accion_buscada)
+                precio_actual = accion_buscada.info["regularMarketPrice"]
+                hablar(f"La encontré, el precio de {accion} es {precio_actual}")
+                continue
+            except:
+                hablar("Perdon pero no la he encontrada")
+                continue
+        elif "adiós" in pedido:
+            hablar("Me voy a descansar, cualquier cosa me avisas")
+
+
+pedir_cosas()
+
+
+
+
+
+
 
 
